dd81ef2f76778adf43c3c38ea68ee3bb9b01fa46
dimapankov <dimaray100500@gmail.com>
2024-09-23 19:47:51 +0700

    последняя версия

--- composeApp/src/commonMain/kotlin/org/example/project/presentation/about_app_feature/viewmodel/intents/AcceptIntent.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/presentation/about_app_feature/viewmodel/intents/AcceptIntent.kt
@@ -3,9 +3,17 @@ package com.project.astranomapp.presentation.about_app_feature.viewmodel.intents
 import com.project.astranomapp.data.StaticDate
 import com.project.astranomapp.presentation.create_user_card_feature.screen.CreateUserCardScreen
 import com.project.astranomapp.presentation.menu_feature.screen.MenuScreen
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.launch
+import org.example.project.ServiceLocator
 
 object AcceptIntent {
     fun execute(){
+        CoroutineScope(Dispatchers.IO).launch {
+            ServiceLocator.key.saveString("name","name")
+        }
         StaticDate.navigator.push(MenuScreen)
     }
 }
